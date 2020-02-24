9e3324de0f3853824e3f134046a7f7af52dafa3e
iggy12345 <aaronjencks@gmail.com>
2020-02-23 20:50:36 -0600

    Imported files

---
+++ main.py
@@ -0,0 +1,102 @@
+from plain import LifePlain
+
+import os
+
+
+def create_glider(r: int, c: int, plain: LifePlain) -> LifePlain:
+    """Inserts a simple glider at the given row and column"""
+
+    plain.construct(r, c, [
+        [False, False, True],
+        [True, False, True],
+        [False, True, True]
+    ])
+
+    return plain
+
+
+def create_generator(r: int, c: int, plain: LifePlain) -> LifePlain:
+    """Inserts a small generator at the given row and column"""
+
+    plain.construct(r, c, [
+        [True, True, True, False, True],
+        [True, False, False, False, False],
+        [False, False, False, True, True],
+        [False, True, True, False, True],
+        [True, False, True, False, True]
+    ])
+
+    return plain
+
+
+def create_gun(r: int, c: int, plain: LifePlain) -> LifePlain:
+    """Creates a simple glider gun"""
+
+    bp = [[False for _ in range(37)] for _ in range(9)]
+
+    bp[0][24] = True
+
+    bp[1][22] = True
+    bp[1][24] = True
+
+    bp[2][12] = True
+    bp[2][13] = True
+    bp[2][20] = True
+    bp[2][21] = True
+    bp[2][34] = True
+    bp[2][35] = True
+
+    bp[3][11] = True
+    bp[3][15] = True
+    bp[3][20] = True
+    bp[3][21] = True
+    bp[3][34] = True
+    bp[3][35] = True
+
+    bp[4][0] = True
+    bp[4][1] = True
+    bp[4][10] = True
+    bp[4][16] = True
+    bp[4][20] = True
+    bp[4][21] = True
+
+    bp[5][0] = True
+    bp[5][1] = True
+    bp[5][10] = True
+    bp[5][14] = True
+    bp[5][16] = True
+    bp[5][17] = True
+    bp[5][22] = True
+    bp[5][24] = True
+
+    bp[6][10] = True
+    bp[6][16] = True
+    bp[6][24] = True
+
+    bp[7][11] = True
+    bp[7][15] = True
+
+    bp[8][12] = True
+    bp[8][13] = True
+
+    plain.construct(r, c, bp)
+
+    return plain
+
+
+if __name__ == '__main__':
+    try:
+        termsize = os.get_terminal_size()
+        p = LifePlain(termsize.lines, termsize.columns)
+        # create_generator(termsize.line // 2, termsize.cloumns // 2, p)
+    except Exception:
+        p = LifePlain(10, 10)
+
+    # create_glider(1, 1, p)
+
+    create_gun(10, 20, p)
+
+    while True:
+        print('\033[2J\033[H' + str(p), end='')
+        p.tick()
+        # input('Press enter to continue')
---
+++ plain.py
@@ -0,0 +1,95 @@
+from populant import LifePopulant
+
+import numpy as np
+
+from multiprocessing import Process, Queue, Pool
+from queue import Empty, Full
+import os
+import time
+from itertools import repeat, product
+
+
+def compute_single(plain: list, coord: tuple) -> bool:
+    """Takes a coordinate tuple (r, c) and computes if that populant will be alive in the next tick."""
+
+    neighbors = 0
+    r, c = coord
+
+    # print('Checking index ({}, {}) of plain that is {}x{}'.format(r, c, len(plain), len(plain[0])))
+
+    t = r > 0
+    b = r < len(plain) - 1
+    l = c > 0
+    rr = c < len(plain[0]) - 1 if len(plain) > 0 else False
+
+    if t:
+        neighbors += 1 if plain[r - 1][c] else 0
+        if rr:
+            neighbors += 1 if plain[r - 1][c + 1] else 0
+        if l:
+            neighbors += 1 if plain[r - 1][c - 1] else 0
+    if b:
+        neighbors += 1 if plain[r + 1][c] else 0
+        if rr:
+            neighbors += 1 if plain[r + 1][c + 1] else 0
+        if l:
+            neighbors += 1 if plain[r + 1][c - 1] else 0
+    if l:
+        neighbors += 1 if plain[r][c - 1] else 0
+    if rr:
+        neighbors += 1 if plain[r][c + 1] else 0
+    # neighbors.append(plain[r][c])
+
+    if neighbors > 3:
+        return False
+    elif neighbors == 3:
+        return True
+    elif neighbors == 2:
+        return plain[r][c]
+    else:
+        return False
+
+
+class LifePlain:
+    """
+    Represents a plain that gets simulated.
+    """
+
+    def __init__(self, height, width):
+        self.board = [[False for _ in range(width)] for _ in range(height)]
+        self.height = height
+        self.width = width
+        self.area = height * width
+
+        self.pool = Pool(os.cpu_count())  # This is the pool used for multiprocessing
+
+        rows = range(self.height)
+        cols = range(self.width)
+        self.coords = list(product(rows, cols, repeat=1))
+
+    def __del__(self):
+        self.pool.close()  # It's very important to close this to release the processes
+
+    def construct(self, r: int, c: int, blueprint: list):
+        """Sets the states of the populants according to the contents of the blueprint (a 2d array of true/false values)
+        Starting at the row and column specified."""
+
+        for rr, rv in enumerate(blueprint):
+            for cc, v in enumerate(rv):
+                if r + rr >= self.height or c + cc >= self.width:
+                    print('Blueprint too large, skipping extra coordinates')
+                self.board[r + rr][c + cc] = v
+
+    def tick(self):
+        """Computes the next board state of the plain, uses multi-core processing to accelerate processing"""
+
+        self.board = np.resize(self.pool.starmap(compute_single, product([self.board], self.coords, repeat=1)),
+                               (self.height, self.width))
+
+    def __str__(self) -> str:
+        result = ''
+        for r in self.board:
+            for p in r:
+                result += '█' if p else ' '
+            result += '\033[E'
+        return result
